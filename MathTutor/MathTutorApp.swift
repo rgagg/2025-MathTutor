7d26ec57f07b1d825b4be622259f01ce254fe32b
Richard Gagg <r.gagg@outlook.com>
2026-02-04 10:38:27 +1100

    Initial Commit

---
+++ MathTutor/MathTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathTutorApp.swift
+//  MathTutor
+//
+//  Created by Richard Gagg on 4/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct MathTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
